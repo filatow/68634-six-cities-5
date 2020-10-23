bab3ea23141672f6b000419174ed2517008bf515
evgeniy <not-found@inbox.ru>
2020-10-23 20:01:28 +0300

    Правки по прошлому модулю

--- src/components/new-comment-form/new-comment-form.jsx
+++ src/components/new-comment-form/new-comment-form.jsx
@@ -1,4 +1,7 @@
-import React, {PureComponent} from "react";
+import React, {PureComponent, Fragment} from "react";
+import {getUniqueId} from "../../utils";
+import {Grade} from "../../consts";
+
 
 export default class NewCommentForm extends PureComponent {
   constructor() {
@@ -22,58 +25,34 @@ export default class NewCommentForm extends PureComponent {
   }
 
   render() {
+    const ratingRadioGroup = Object.values(Grade).map((grade, ind, arr) => {
+      return (
+        <Fragment
+          key={getUniqueId()}>
+          <input
+            className="form__rating-input visually-hidden"
+            name="rating" id={`${arr.length - ind}-stars`} type="radio"
+            value={arr.length - ind}
+            onChange={this._handleFieldChange}
+            checked={this.state.rating === String(arr.length - ind)}/>
+          <label htmlFor={`${arr.length - ind}-stars`} className="reviews__rating-label form__rating-label" title={grade}>
+            <svg className="form__star-image" width="37" height="33">
+              <use xlinkHref="#icon-star"></use>
+            </svg>
+          </label>
+        </Fragment>
+      );
+    });
     return (
       <form className="reviews__form form" action="#" method="post">
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
         <div className="reviews__rating-form form__rating">
-          <input
-            className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"
-            onChange={this._handleFieldChange}/>
-          <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input
-            className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"
-            onChange={this._handleFieldChange}/>
-          <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input
-            className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"
-            onChange={this._handleFieldChange}/>
-          <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input
-            className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"
-            onChange={this._handleFieldChange}/>
-          <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input
-            className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"
-            onChange={this._handleFieldChange}/>
-          <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
+          {ratingRadioGroup}
         </div>
         <textarea
           className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"
-          onChange={this._handleFieldChange}></textarea>
+          onChange={this._handleFieldChange}
+          value={this.state.review}></textarea>
         <div className="reviews__button-wrapper">
           <p className="reviews__help">
             To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
--- src/consts.js
+++ src/consts.js
@@ -4,3 +4,11 @@ export const OfferType = {
   HOUSE: `House`,
   HOTEL: `Hotel`
 };
+
+export const Grade = {
+  PERFECT: `perfect`,
+  GOOD: `good`,
+  NOT_BAD: `not bad`,
+  BADLY: `badly`,
+  TERRIBLY: `terribly`,
+};
